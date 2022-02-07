ee082e15d3b4ff35fe22bced7ebfe504481f80c4
Kim Almroth <almroth.kim@gmail.com>
2022-02-07 14:40:39 +0100

    add fading names to startpage

--- script.js
+++ script.js
@@ -1,89 +1,30 @@
-// function menuToggle() {
-// var x = document.getElementById("links");
-// if (x.style.display === "flex") {
-// hideMenu();
-// } else {
-// showMenu();
-// }
-// }
-// function showMenu() {
-// var y = document.getElementById("topNav");
-// var x = document.getElementById("links");
-
-// x.style.display = "flex";
-// y.style.backgroundColor = "#111111";
-// x.style.border = "1px solid #222222";
-// menuHidden = false;
-// }
-// function hideMenu() {
-// var y = document.getElementById("topNav");
-// var x = document.getElementById("links");
-
-// x.style.display = "none";
-// y.style.backgroundColor = "transparent";
-// x.style.border = "hidden";
-// menuHidden = true;
-// }
-// window.onresize = resize;
-// let menuHidden = false;
-
-// function resize() {
-//   var desktop = window.matchMedia("(min-width: 768px)");
-//   var mobile = window.matchMedia("(max-width: 767px)");
-//   if (menuHidden && desktop.matches) {
-//     showMenu();
-//   } else if (menuHidden === false && mobile.matches) {
-//     hideMenu();
-//   }
-// }
 let topExpanded = false;
-let topShrinked = false;
-let BottomExpanded = false;
-let bottomShrinked = false;
+let rightExpanded = false;
+let bottomExpanded = false;
+let leftExpanded = false;
 let mainAnimationFinished = true;
 
-function fadeInTopText(){
-  document.getElementById("topText").style.animation = "fadeIn 2s forwards";
-  document.getElementById("topName").style.animation = "fadeIn 2s forwards";
-  document.getElementById("topLink").style.animation = "fadeIn 2s forwards";
-  document.getElementById("topText").style.pointerEvents = "auto";
-  document.getElementById("topName").style.pointerEvents = "auto";
-  document.getElementById("topLink").style.pointerEvents = "auto";
-  document.getElementById("allTopText").style.backgroundColor = "rgba(0,0,0,0.25)";
+function fadeInText(parent) {
+  const children = Array.from(parent.children);
+
+  children.forEach((element) => {
+    element.style.animation = "fadeIn 2s forwards";
+    element.style.pointerEvents = "auto";
+    element.style.backgroundColor = "rgba(0,0,0,0.25)";
+  });
 }
-function fadeOutTopText(){
-  document.getElementById("topText").style.animation = "fadeOut 2s forwards";
-  document.getElementById("topName").style.animation = "fadeOut 2s forwards";
-  document.getElementById("topLink").style.animation = "fadeOut 2s forwards";
-  document.getElementById("topText").style.pointerEvents = "none";
-  document.getElementById("topName").style.pointerEvents = "none";
-  document.getElementById("topLink").style.pointerEvents = "none";
-  document.getElementById("allTopText").style.backgroundColor = "rgba(0,0,0,0)";
+function fadeOutText(parent) {
+  const children = Array.from(parent.children);
+
+  children.forEach((element) => {
+    element.style.animation = "fadeOut 2s forwards";
+    element.style.pointerEvents = "none";
+    element.style.backgroundColor = "rgba(0,0,0,0)";
+  });
 }
-function fadeInBottomText(){
-  document.getElementById("bottomText").style.animation = "fadeIn 2s forwards";
-  document.getElementById("bottomName").style.animation = "fadeIn 2s forwards";
-  document.getElementById("bottomLink").style.animation = "fadeIn 2s forwards";
-  document.getElementById("bottomText").style.pointerEvents = "auto";
-  document.getElementById("bottomName").style.pointerEvents = "auto";
-  document.getElementById("bottomLink").style.pointerEvents = "auto";
-  document.getElementById("allBottomText").style.backgroundColor = "rgba(0,0,0,0.25)";
-}
-function fadeOutBottomText(){
-  document.getElementById("bottomText").style.animation = "fadeOut 2s forwards";
-  document.getElementById("bottomName").style.animation = "fadeOut 2s forwards";
-  document.getElementById("bottomLink").style.animation = "fadeOut 2s forwards";
-  document.getElementById("bottomText").style.pointerEvents = "none";
-  document.getElementById("bottomName").style.pointerEvents = "none";
-  document.getElementById("bottomLink").style.pointerEvents = "none";
-  document.getElementById("allBottomText").style.backgroundColor = "rgba(0,0,0,0)";
-}
+
 function toggleTop() {
-  // alert("I am an alert box!");
   let top = document.getElementById("top");
-  let bottom = document.getElementById("bottom");
-  let topText = document.getElementById("topText");
-  let bottomText = document.getElementById("bottomText");
   let welcome = document.getElementById("welcome");
 
   top.addEventListener("animationstart", (ev) => {
@@ -93,50 +34,28 @@ function toggleTop() {
     mainAnimationFinished = true;
   });
 
-  if (!topExpanded && !BottomExpanded && mainAnimationFinished) {
-    top.style.animation = "topFromNormalToExpand 2s forwards";
-    bottom.style.animation = "bottomFromNormalToShrink 2s forwards";
-
+  if (!topExpanded && mainAnimationFinished) {
+    top.style.zIndex = "2";
+    top.style.animation = "topExpand 2s forwards";
     topExpanded = true;
-    fadeInTopText();
-    welcome.style.animation = "fadeOut 1s forwards";
-  } else if (BottomExpanded && mainAnimationFinished) {
-    top.style.animation = "topFromShrinkToExpand 1s forwards";
-    bottom.style.animation = "bottomFromExpandToShrink 1s forwards";
-
-    topExpanded = true;
-    BottomExpanded = false;
-
-    fadeOutBottomText();
-    fadeInTopText();
+    fadeInText(document.getElementById("allTopText"));
     welcome.style.animation = "fadeOut 1s forwards";
   } else if (mainAnimationFinished) {
-    top.style.animation = "topFromExpandToNormal 2s forwards";
-    bottom.style.animation = "bottomFromShrinkToNormal 2s forwards";
-
     topExpanded = false;
 
-    fadeOutBottomText();
-    fadeOutTopText();
+    top.style.animation = "topShrink 2s forwards";
+    fadeOutText(document.getElementById("allTopText"));
     welcome.style.animation = "fadeIn 2s forwards";
 
-    top.blur();
+    setTimeout(function () {
+      top.style.zIndex = "1";
+      top.blur();
+    }, 2000);
   }
-}
-function test() {
-  let top = document.getElementById("top");
-  let bottom = document.getElementById("bottom");
-  let text = document.getElementById("topText");
-
-  text.scrollBy(0,0);
-  
 }
 
 function toggleBottom() {
-  let top = document.getElementById("top");
   let bottom = document.getElementById("bottom");
-  let topText = document.getElementById("topText");
-  let bottomText = document.getElementById("bottomText");
   let welcome = document.getElementById("welcome");
 
   bottom.addEventListener("animationstart", (ev) => {
@@ -146,34 +65,89 @@ function toggleBottom() {
     mainAnimationFinished = true;
   });
 
-  if (!BottomExpanded && !topExpanded && mainAnimationFinished) {
-    bottom.style.animation = "bottomFromNormalToExpand 2s forwards";
-    top.style.animation = "topFromNormalToShrink 2s forwards";
-
-    BottomExpanded = true;
-
-    fadeInBottomText();
-    welcome.style.animation = "fadeOut 1s forwards";
-  } else if (topExpanded && mainAnimationFinished) {
-    bottom.style.animation = "bottomFromShrinkToExpand 1s forwards";
-    top.style.animation = "topFromExpandToShrink 1s forwards";
-
-    BottomExpanded = true;
-    topExpanded = false;
-
-    fadeInBottomText();
-    fadeOutTopText();
+  if (!bottomExpanded && mainAnimationFinished) {
+    bottom.style.zIndex = "2";
+    bottom.style.animation = "bottomExpand 2s forwards";
+    bottomExpanded = true;
+    fadeInText(document.getElementById("allBottomText"));
     welcome.style.animation = "fadeOut 1s forwards";
   } else if (mainAnimationFinished) {
-    bottom.style.animation = "bottomFromExpandToNormal 2s forwards";
-    top.style.animation = "topFromShrinkToNormal 2s forwards";
-
-    BottomExpanded = false;
-
-    fadeOutBottomText();
-    fadeOutTopText();
+    bottomExpanded = false;
+    bottom.style.animation = "bottomShrink 2s forwards";
+    fadeOutText(document.getElementById("allBottomText"));
     welcome.style.animation = "fadeIn 2s forwards";
 
-    bottom.blur();
+    setTimeout(function () {
+      bottom.style.zIndex = "1";
+      bottom.blur();
+    }, 2000);
   }
+}
+
+function toggleLeft() {
+  let left = document.getElementById("left");
+  let welcome = document.getElementById("welcome");
+
+  left.addEventListener("animationstart", (ev) => {
+    mainAnimationFinished = false;
+  });
+  left.addEventListener("animationend", (ev) => {
+    mainAnimationFinished = true;
+  });
+
+  if (!leftExpanded && mainAnimationFinished) {
+    left.style.zIndex = "2";
+    left.style.animation = "leftExpand 2s forwards";
+    leftExpanded = true;
+    fadeInText(document.getElementById("allLeftText"));
+    welcome.style.animation = "fadeOut 1s forwards";
+  } else if (mainAnimationFinished) {
+    leftExpanded = false;
+    left.style.animation = "leftShrink 2s forwards";
+    fadeOutText(document.getElementById("allLeftText"));
+    welcome.style.animation = "fadeIn 2s forwards";
+
+    setTimeout(function () {
+      left.style.zIndex = "1";
+      left.blur();
+    }, 2000);
+  }
+}
+
+function toggleRight() {
+  let right = document.getElementById("right");
+  let welcome = document.getElementById("welcome");
+
+  right.addEventListener("animationstart", (ev) => {
+    mainAnimationFinished = false;
+  });
+  right.addEventListener("animationend", (ev) => {
+    mainAnimationFinished = true;
+  });
+
+  if (!rightExpanded && mainAnimationFinished) {
+    right.style.zIndex = "2";
+    right.style.animation = "rightExpand 2s forwards";
+    rightExpanded = true;
+    fadeInText(document.getElementById("allRightText"));
+    welcome.style.animation = "fadeOut 1s forwards";
+  } else if (mainAnimationFinished) {
+    rightExpanded = false;
+    right.style.animation = "rightShrink 2s forwards";
+    fadeOutText(document.getElementById("allRightText"));
+    welcome.style.animation = "fadeIn 2s forwards";
+
+    setTimeout(function () {
+      right.style.zIndex = "1";
+      right.blur();
+    }, 2000);
+  }
+}
+
+function test() {
+  let top = document.getElementById("top");
+  let bottom = document.getElementById("bottom");
+  let text = document.getElementById("topText");
+
+  text.scrollBy(0, 0);
 }
