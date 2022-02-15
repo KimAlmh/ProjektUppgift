79d9c3f6a1953417c8dec77b1d38973b49e765a2
Kim Almroth <almroth.kim@gmail.com>
2022-02-15 19:17:37 +0100

    working real time email validation

--- kontakt.js
+++ kontakt.js
@@ -1,36 +1,38 @@
 let isEmailCorrect = false;
 
 function validateEmail() {
-    var emailValue = document.getElementById("email").value;
-    var email = document.getElementById("email");
-    var button = document.getElementById("submit");
-    var re = /^\b[A-ZÅÄÖåäö0-9._%+-]+@[A-ZÅÄÖåäö0-9.-]+\.[A-Z]{2,4}\b$/i;
-    if (re.test(emailValue)) {
-        email.style.border = "1px solid #e6e6e6";
-        isEmailCorrect = true;
-        button.style.backgroundColor = "#00ad5f";
-        button.style.pointerEvents = "auto";
-    }
-    else if (emailValue === ""){
-        email.style.border = "1px solid #e6e6e6";
-        isEmailCorrect = false;
+  var emailValue = document.getElementById("email").value;
+  var email = document.getElementById("email");
+  var button = document.getElementById("submit");
+  var emailError = document.getElementById("emailError");
+  var re = /^\b[A-ZÅÄÖåäö0-9._%+-]+@[A-ZÅÄÖåäö0-9.-]+\.[A-Z]{2,4}\b$/i;
+  if (re.test(emailValue)) {
+    email.style.border = "1px solid #e6e6e6";
+    isEmailCorrect = true;
+    button.style.backgroundColor = "#00ad5f";
+    button.style.pointerEvents = "auto";
+    emailError.style.display = "none";
+  } else if (emailValue === "") {
+    email.style.border = "1px solid #e6e6e6";
+    isEmailCorrect = false;
 
-        button.style.backgroundColor = "grey";
-        button.style.pointerEvents = "none";
-    }
-    else {
-        email.style.border = "red solid 2px";
-        isEmailCorrect = false;
-        button.style.backgroundColor = "grey";
-        button.style.pointerEvents = "none";
-    }
+    emailError.style.display = "none";
+    button.style.backgroundColor = "grey";
+    button.style.pointerEvents = "none";
+  } else {
+    email.style.border = "red solid 2px";
+    isEmailCorrect = false;
+    button.style.backgroundColor = "grey";
+    button.style.pointerEvents = "none";
+    emailError.style.display = "flex";
+  }
 }
 
-function validateSubmit(){
-    if(!isEmailCorrect){
-        alert("Email not correct!");
-        return false;
-    }
-    return true;
-
+function validateSubmit() {
+  if (!isEmailCorrect) {
+    var emailError = document.getElementById("emailError");
+    emailError.style.display = "flex";
+    return false;
+  }
+  return true;
 }
